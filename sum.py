10be448901e89dac0c0cb2aa76251cdff59d9101
1U23CS056KAVIN <139535763+1U23CS056KAVIN@users.noreply.github.com>
2026-01-09 04:31:51 +0000

    1

---
+++ sum.py
@@ -0,0 +1,14 @@
+a=int(input("enter the number:"))
+b=int(input("enter the number:"))
+c=a+b
+print(c)
+
+#with function
+def add_numbers(a, b):
+    return a+b
+num1=int(input("Enter first number:"))
+num2=int(input("Enter second number:"))
+result=add_numbers(num1,num2)
+print("the sum of the two numbers is:",result)
+    
+   
