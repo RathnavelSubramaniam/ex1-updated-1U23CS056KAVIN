6be599892e7cb0c963c5d88ffdd7213b46ed55f3
1U23CS056KAVIN <139535763+1U23CS056KAVIN@users.noreply.github.com>
2026-01-09 07:27:33 +0000

    3

---
+++ squareroot.py
@@ -0,0 +1,3 @@
+num = int(input("enter a square root:"))
+result = pow(num, 0.5)
+print(result)
---
+++ sum2.py
@@ -0,0 +1,20 @@
+a = int(input("enter the swap number:"))
+b = int(input("enter the swap number:"))
+temp = a
+a=b
+b=temp
+print("after swapping")
+print(a)
+print(b)
+
+#with function
+def swap(a,b):
+    return a
+a = int(input("enter the swap number:"))
+b = int(input("enter the swap number:"))
+temp = a
+a=b
+b=temp
+print("after swapping")
+print(a)
+print(b)
